e7c08f58c104c33fe569f485484b3c0a3fb5717f
Jahanvi Dubey <55359644+Jahanvidubey@users.noreply.github.com>
2020-04-01 01:58:30 -0600

    Update sneh.py

--- vs with git/snehjavi/sneh.py
+++ vs with git/snehjavi/sneh.py
@@ -1,6 +1,9 @@
-n=100
+n=10
 for i in range(n):
     print("USnehjaviL")
     n+=100
     #how to infinite loop DB jahanvi
 
+for j in range(n):
+    print("gpagapgpagapp haahahahhah")
+    print("next time we enjoy a lot")
